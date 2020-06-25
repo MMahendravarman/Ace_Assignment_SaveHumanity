ea45eb4eec4647cacce87104b4ecabce8852c5d9
pughazhenthi m <pughazhenthi m>
2020-06-25 18:23:01 +0530

    deleted dups

--- blood-donor-ws/mahendravarman-blood-donor-ws-37bb121b8462/src/main/java/com/hcl/blooddonorws/data/BloodDonorsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.blooddonorws.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-
-public interface BloodDonorsRepository extends CrudRepository<BloodDonorsEntity, String>{
-
-}
